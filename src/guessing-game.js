fc67e491a6172366ff42755aaf74070246141bf9
molestador <hustlerxxl@gmail.com>
2017-01-30 13:31:54 +0200

    guessing-game/feature : implement all methods

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
+        this.max = max;
+        this.min = min;
 
     }
 
     guess() {
+        var nubmer = Math.round((this.min + this.max) / 2);
+        return nubmer;
 
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max) / 2);
     }
 
     greater() {
+        this.min = Math.round((this.min + this.max) / 2);
 
     }
 }
